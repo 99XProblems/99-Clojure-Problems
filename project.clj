2782c417a2c8df6d70516894db3bfd576cdb945c
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-24 19:28:58 +0100

    URL & description added

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject p99 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "99 Clojure Problems"
+  :url "https://github.com/99XProblems/99-Clojure-Problems"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
