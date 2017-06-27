dfc762af6384be77e2604ab89812a762a8d7f908
Baptiste Fontaine <b@ptistefontaine.fr>
2017-06-27 20:16:20 +0200

    Bump Clojure

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/99XProblems/99-Clojure-Problems"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
